aba79519e127d650fa78c6d20b9a48df036d958f
jonas-pettersson <j.g.f.pettersson@gmail.com>
2014-05-10 22:27:45 +0200

    update

--- PowersOfTwo.cpp
+++ PowersOfTwo.cpp
@@ -4,3 +4,13 @@
  * This program generates a list of the powers of
  * two up to an exponent limit entered by the user
  */
+
+#include <iostream>
+using namespace std;
+
+/* Function prototypes */
+
+int raiseToPower(int n, int k);
+
+/* Main program */
+
