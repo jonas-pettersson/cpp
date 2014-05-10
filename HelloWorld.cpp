12f087c47df880b18a64b718ae451df42cbda1e7
jonas-pettersson <j.g.f.pettersson@gmail.com>
2014-05-10 21:56:04 +0200

    C++ examples

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+/*
+ * File: HelloWorld.cpp
+ * -------------------
+ * 
+*/
+# include <iostream>
+using namespace std;
+
+int main() {
+  cout << "hello, world" << endl;
+  return 0;
+}
---
+++ PowersOfTwo.cpp
@@ -0,0 +1,6 @@
+/*
+ * File: PowersOfTwo.cpp
+ * ---------------------
+ * This program generates a list of the powers of
+ * two up to an exponent limit entered by the user
+ */
