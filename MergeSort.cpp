d5c98c061d444cea07de54d259b92c5fe0491132
jonas-pettersson <j.g.f.pettersson@gmail.com>
2014-05-12 13:12:23 +0200

    added merge sort (draft)

---
+++ MergeSort.cpp
@@ -0,0 +1,13 @@
+/*
+ * File: MergeSort.cpp
+ * -------------------
+ * This programs sorts an input array
+ * of integers using the merge sort algorithm.
+ */
+
+#include <iostream>
+using namespace std;
+
+/* Main program */
+
+int main() {
