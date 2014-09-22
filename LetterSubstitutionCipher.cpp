126e805f2daf1f0a6363615054e140e603ddbada
jonas-pettersson <j.g.f.pettersson@gmail.com>
2014-09-22 21:11:58 +0200

    Added invertKey function

--- LetterSubstitutionCipher.cpp
+++ LetterSubstitutionCipher.cpp
@@ -14,24 +14,36 @@
 using namespace std;
 
 /* Function prototypes */
-string encodeLetterSubstitutionCipher(string str, string translation);
+string encodeLetterSubstitutionCipher(string str, string encryptionKey);
+string invertKey(string encryptionKey);
 
 /* Main program */
 int main() {
   cout << "Letter Substitution Cipher." << endl;
-  string translation = getLine("Enter a 26-letter key: ");
+  string encryptionKey = getLine("Enter a 26-letter key: ");
   string message = getLine("Enter a message: ");
-  cout << "Encoded message: " << encodeLetterSubstitutionCipher(message, translation) << endl;
+  string encodedMessage = encodeLetterSubstitutionCipher(message, encryptionKey);
+  cout << "Encoded message: " << encodedMessage << endl;
+  string decryptionKey = invertKey(encryptionKey);
+  cout << "Decryption key: " << decryptionKey << endl;
+  cout << "Decrypted message: " << encodeLetterSubstitutionCipher(encodedMessage, decryptionKey) << endl;
   return 0;
 }
 
 
-string encodeLetterSubstitutionCipher(string str, string translation) {
-  char newChar;
+string encodeLetterSubstitutionCipher(string str, string encryptionKey) {
   for (int i = 0; i < str.length(); i++) {
     if (isupper(str[i])) {
-      str[i] = translation[str[i] - 'A'];
+      str[i] = encryptionKey[str[i] - 'A'];
     }
   }
   return str;
+}
+
+string invertKey(string encryptionKey) {
+  string decryptionKey = string(26, ' ');
+  for (int i = 0; i < encryptionKey.length(); i++) {
+    decryptionKey[encryptionKey[i] - 'A'] = 'A' + i;
+  }
+  return decryptionKey;
 }
